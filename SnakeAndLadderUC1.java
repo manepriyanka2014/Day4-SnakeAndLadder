de848c12bc6f50f365d07130c6331455131c49e4
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-04 12:35:56 +0530

    UC1-PlayerPositionAt0

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,9 @@
+class SnakeAndLadderUC1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Snake and Ladder Game");
+		int playerPosition=0;
+		System.out.println("Snake and Ladder game starting position=" +playerPosition);
+	}
+}""
