6aecda3ec7311452fca750bd2148b1e96ef57e88
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-04 12:43:21 +0530

    Player checks option No Play Ladder or Snake

---
+++ SnakeAndLadderUC3.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+public class SnakeAndLadderUC3{
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game ");
+        int playerPosition=0;
+        System.out.println("Starting player position is "+playerPosition+" ; Player rolls die");
+        Random rand=new Random();
+        int rollOutput,option;
+
+            rollOutput = rand.nextInt(6);
+            System.out.println("Die output : " + (rollOutput+1));
+        //checking option No play, Snake and Ladder
+        option=rand.nextInt(3);
+        
+        switch (option){
+            case 0:
+                System.out.println("No Play");
+                playerPosition=playerPosition+0;
+                System.out.println(playerPosition);
+                break;
+            case 1:
+                System.out.println("Snake");
+                playerPosition= playerPosition-rollOutput+1;
+                if (playerPosition<0){
+                    playerPosition=0;
+                    System.out.println(playerPosition);
+                }else{
+                System.out.println(playerPosition);}
+                break;
+            case 2:
+                System.out.println("Ladder");
+                playerPosition=playerPosition+rollOutput+1;
+                System.out.println(playerPosition);
+                break;
+        }
+
+    }
+}
