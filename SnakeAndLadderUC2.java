eb422a5e49307034eb6d8c03475e5934c6134d13
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-04 12:38:46 +0530

    Player Rolls Die Between 1 to 6

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class SnakeAndLadderUC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game ");
+        int playerPosition=0;
+        System.out.println("Starting player position is "+playerPosition+" ; Player rolls die");
+        Random rand=new Random();
+        int rollOutput;
+        rollOutput= rand.nextInt(6);
+        if (rollOutput ==0){
+            rollOutput =1;
+        }
+        System.out.println("Die output : "+rollOutput);
+    }
+}
